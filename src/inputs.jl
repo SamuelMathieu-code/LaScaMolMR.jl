7fda3a7f3d9c45aeb77a9ca0ae41f3625ff84672
SamuelMathieu-code <samuelmathieu@zoho.com>
2023-03-04 01:36:32 -0500

    path update

--- src/inputs.jl
+++ src/inputs.jl
@@ -47,19 +47,18 @@ end
 """
 Return Regex from Vector of Strings and ValTypeGen.GenVarInfo
 """
-function make_regex_path(path1::String, path2::Vector{Any})::Regex
+function make_regex_path(path::Vector{Any})::Regex
     str = "";
-    for i in path2
+    for i in path
         if (i isa String); str*=i; else; str*="(.)"; end;     # pourra etre augmenté pour prendre en compte les types d'infos et les conditions a respecter.
     end
-    str = joinpath(path1, str);
     return Regex(str)
 end
 
 """
 Return all existing files corresponding to the regex defined
 """
-function find_all_corresp(path1::String, path2::Vector{Any})::Vector{String}
+function find_all_corresp(path::Vector{Any})::Vector{String}
     #...
     return []
 end
@@ -68,7 +67,7 @@ end
 """
 Find all existing files corresponding to the regex defined and constrait on gene names.
 """
-function find_all_corresp(path1::String, path2::Vector{Any}, 
+function find_all_corresp(path2::Vector{Any}, 
                           genes::Vector{String})::Vector{String}
     #...
     return []
@@ -78,30 +77,27 @@ end
 """
 Find all existing files corresponding to the regex defined and constrait on gene names and chromosomes.
 """
-function find_all_corresp(path1::String, path2::Vector{Any}, 
+function find_all_corresp(path2::Vector{Any}, 
                           genes::Vector{String},
                           chr::Vector{String})::Vector{String}
     #...
     return []
 end
 
-QtlStudy(path1::String, 
-         path2::Vector{Any}, 
+QtlStudy(path::Vector{Any}, 
          columns::Vector{ValTypeGen.GenVarInfo}, 
-         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path1, path2), columns, separator)
+         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path), columns, separator)
 
-QtlStudy(path1::String, 
-         path2::Vector{Any},
+QtlStudy(path::Vector{Any},
          genes::Vector{String},
          columns::Vector{ValTypeGen.GenVarInfo}, 
-         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path1, path2, genes), columns, separator)
+         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path, genes), columns, separator)
 
-QtlStudy(path1::String, 
-         path2::Vector{Any},
+QtlStudy(path::Vector{Any},
          genes::Vector{String},
          chr::Vector{String},
          columns::Vector{ValTypeGen.GenVarInfo}, 
-         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path1, path2, genes, chr), columns, separator)
+         separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path, genes, chr), columns, separator)
 
 # Iteration overload
 function Base.iterate(iter::QtlStudy)
