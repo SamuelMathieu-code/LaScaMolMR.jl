2d8cedbb03d677d1598ca514de8f7462dae6caf0
SamuelMathieu-code <samuelmathieu@zoho.com>
2023-03-10 18:10:36 -0500

    added access function for GWAS structure

--- src/inputs.jl
+++ src/inputs.jl
@@ -13,10 +13,12 @@
     RSID
     CHR_POS
     CHR_COLONG_POS
-    β
+    BETA
     SE
     MAF
     ODD_RATIO
+    CI
+    PVAL
     # add others ?
     OTHER_INFO
 end
@@ -35,30 +37,76 @@ struct GWAS
     columns::Vector{GenVarInfo}
     separator::Union{String, Char}
     trait_name::Union{nothing, String}
-    chr::Union{nothing, Int} # Int or string?
+    chr::Union{nothing, Int8} # Int or string?
     trait_iso::Union{nothing, String}
+    acc_func::Function # -----------------------> acces infos from line of file
 end
 
-GWAS(path::String,
-     columns::Vector{GenVarInfo},
-     separator::Union{String, Char}) = GWAS(path, columns, separator, nothing, nothing, nothing)
+
+"""
+Return function that extracts ([chr, pos], [beta, se, pval])
+"""
+function make_func(path::String, 
+                   columns::Vector{GenVarInfo}, 
+                   sep::Union{String, Char}, 
+                   trait_name::Union{String, nothing} = nothing, 
+                   chr::Union{Int8, nothing}=nothing, 
+                   trait_iso::Union{nothing, String}=nothing)::Function
+    
+    # return a function that threats a line (String) and returns a tuple of vectors : ([chr, pos], [beta, se, pval]) --> raise err if not enough info in columns
+    # cas qui foirent -> 
+    #                   si un de chr, trait_iso, trait_name != nothing et pas dans columns -> foire.
+    #                   si ni (β, SE) ou (OR, CI) dans columns -> foire
+    #                   si ni (CHR, POS), CHR_POS, CHR_COLONG_POS dans columns -> foire 
+    
+    if chr!==nothing && !(CHR in columns) && !(CHR_COLONG_POS in columns) && !(CHR_POS in columns); throw(ErrorException("no argument chr in columns of GWAS")); end
+    if trait_name!==nothing && !(TRAIT_NAME in columns) && !(TRAIT_NAME_ISO in columns); throw(ErrorException("no argument TRAIT_NAME in columns of GWAS")); end
+    if trait_iso!==nothing && !(TRAIT_ISO in columns) && !(TRAIT_NAME_ISO in columns); throw(ErrorException("no argument trait_iso in columns of GWAS")); end
+    if !(CHR_COLONG_POS in cloumns) || !(CHR_POS in columns) || !((CHR in columns) && (POS in columns)); throw(ErrorException("Not enough information about variants in columns")); end
+    if !(PVAL in columns); throw(ErrorException("No pvalue info in columns")); end
+    
+    if BETA in columns && SE in columns
+        
+        #...
+    elseif OR in columns && CI in columns
+        
+        #...
+    else
+        throw(ErrorException("Not either (BETA, SE) or (OR, CI) in columns"))
+    end
+
+    return (line::String -> ([0, 0], [0, 0, 0]))
+end
+
 
 GWAS(path::String,
      columns::Vector{GenVarInfo},
-     separator::Union{Char, String}
-     trait_name::String) = GWAS(path, columns, separator, trait_name, nothing, nothing)
+     separator::Union{String, Char}) = GWAS(path, columns, separator, nothing, nothing, nothing, make_func(path, columns, separator))
+
+GWAS(path::String,
+     columns::Vector{GenVarInfo},
+     separator::Union{Char, String},
+     trait_name::String) = GWAS(path, columns, separator, trait_name, nothing, nothing, make_func(path, columns, separator, trait_name))
 
 GWAS(path::String,
      columns::Vector{GenVarInfo},
      separator::Union{String, Char},
      trait_name::String,
-     trait_iso::String) = GWAS(path, columns, separator, trait_name, nothing, trait_iso)
+     trait_iso::String) = GWAS(path, columns, separator, trait_name, nothing, trait_iso, make_func(path, columns, trait_name, nothing, trait_iso))
 
 GWAS(path::String,
      columns::Vector{String},
      separator::Union{String, Char},
      trait_name::String,
-     chr::Int) = GWAS(path, columns, separator, trait_name, chr, nothing)
+     chr::Int) = GWAS(path, columns, separator, trait_name, chr, nothing, make_func(path, columns, trait_name, chr))
+
+GWAS(path::String,
+     columns::Vector{String},
+     separator::Union{String, Char},
+     trait_name::String,
+     chr::Int,
+     trait_iso::String) = GWAS(path, columns, separator, trait_name, chr, trait_iso, make_func(path, columns, trait_name, chr, trait_iso))
+
 
 struct QtlStudy
     paths::Vector{String}
@@ -121,18 +169,20 @@ QtlStudy(path::Vector{QtlPathPattern},
          columns::Vector{ValTypeGen.GenVarInfo}, 
          separator::Union{String, Char}) =  QtlStudy(find_all_corresp(path, genes, chr), columns, separator)
 
-# Iteration overload
+
+# Iteration overload for QtlStudy
 function Base.iterate(iter::QtlStudy)
     element = GWAS(iter.paths[1], iter.columns, iter.separator);
     return (element, 1)
 end
+
 
 function Base.iterate(iter::QtlStudy, state)
     count = state+1;
     if count>length(iter.paths)
         return nothing
     end
-    element = GWAS(iter.paths[count], iter.columns, iter.separator);
+    element = GWAS(iter.paths[count], iter.columns, iter.separator); ##### TO BE MODIF WITH NEW ADDINGS
     return (element, count)
 end
 
--- src/ivSelect.jl
+++ src/ivSelect.jl
@@ -1,9 +1,12 @@
 
+
 PLINK = get(ENV, "PLINK_PATH", Sys.BINDIR) # Get PLINK_PATH as enc variable default being the environement's bin directory.
+
 
 ##################################################
 #                    ivSelect                    #
 ##################################################
+
 
 """
 Find potential ivs from exposure with cis method
@@ -15,6 +18,7 @@ function get_exp_ivs_cis(exposure::GWAS)::Tuple{Array{Int64, 2}, Array{Float64, 
     return [], []
 end
 
+
 """
 Find potential ivs from exposure with trans method
 """
@@ -23,8 +27,9 @@ function get_exp_ivs_trans(exposure::GWAS)::Tuple{Array{Int64, 2}, Array{Float64
     return [], []
 end
 
+
 """
-merge exp iv list with snps available in target + clumping of ivs if corr_treshold is not `nothing`.
+Merge exp iv list with snps available in target + clumping of ivs if corr_treshold is not `nothing` --> PLINK.
 """
 function get_corresp_ivs(target::GWAS, pot_ivs::Array{Int64, 2},  corr_threshold::Union{nothing, Float16} = 0.1)::Tuple{Vector{Int64}, Array{Float64, 2}}
     #...
@@ -42,6 +47,7 @@ function ivSelectCis(exposure::GWAS, target::GWAS, corr_threshold::Union{nothing
     pot_ivs, data_exp = pot_ivs[index], data_exp[index]
     return pot_ivs, data_exp, data_taget
 end
+
 
 """
 Find ivs for MR from `exposure` to `target` using trans method and with maximal correlation treshold between ivs of `corr_threshold`.
